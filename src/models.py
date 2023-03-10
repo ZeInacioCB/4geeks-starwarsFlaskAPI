554c086cb3cbe41a28604271990a3feda13ea896
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-03-10 20:35:23 +0000

    Models built

--- src/models.py
+++ src/models.py
@@ -17,3 +17,37 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planets(db.Model):
+    planet_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    terrain = db.Column(db.String(250))
+    climate = db.Column(db.String(250))
+    population = db.Column(db.String(250))
+    diameter = db.Column(db.String(250))
+
+class Characters(db.Model):
+    character_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    homeworld_id = db.Column(db.Integer, db.ForeignKey(Planets.planet_id))
+    gender = db.Column(db.String(10))
+    hairColor = db.Column(db.String(250))
+    skinColor = db.Column(db.String(250))
+    eyeColor = db.Column(db.String(250))
+
+class Starships(db.Model):
+    starship_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    pilot_id = db.Column(db.Integer, db.ForeignKey(Characters.character_id))
+    model = db.Column(db.String(500))
+    manufacturer = db.Column(db.String(250))
+    cost = db.Column(db.Integer)
+    topSpeed = db.Column(db.Integer)
+    maxCargo = db.Column(db.Integer)
+
+class Favourites(db.Model):
+    favourite_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    character_id = db.Column(db.Integer, db.ForeignKey(Characters.character_id))
+    planet_id = db.Column(db.Integer, db.ForeignKey(Planets.planet_id))
+    starship_id = db.Column(db.Integer, db.ForeignKey(Starships.starship_id))
